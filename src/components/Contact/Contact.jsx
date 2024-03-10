9c259262c40e4db432d4fd94d411fe4eb1690b45
Muhammad Haris Khan <hariskhan873715@gmail.com>
2024-03-10 13:25:39 +0500

    Portfolio-Website

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,18 +13,20 @@ export const Contact = () => {
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:myemail@email.com">myemail@email.com</a>
+          <a href="mailto:hariskhan873715@gmail.com">hariskhan873715@email.com</a>
         </li>
         <li className={styles.link}>
           <img
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="https://www.linkedin.com/myname">linkedin.com/myname</a>
+          <a href="https://www.linkedin.com/in/muhammad-khan-a55211286/">
+            linkedin.com/Haris-khan
+          </a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://www.github.com/myname">github.com/myname</a>
+          <a href="https://github.com/Hariskhan000/">github.com/Haris-khan</a>
         </li>
       </ul>
     </footer>
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,9 +7,9 @@ export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I'm Ada</h1>
+        <h1 className={styles.title}>Hi, I'm Haris</h1>
         <p className={styles.description}>
-          I'm a full-stack developer with 5 years of experience using React and
+          I'm a Mern-stack developer with 1 year of experience using React and
           NodeJS. Reach out if you'd like to learn more!
         </p>
         <a href="mailto:myemail@email.com" className={styles.contactBtn}>
@@ -17,7 +17,7 @@ export const Hero = () => {
         </a>
       </div>
       <img
-        src={getImageUrl("hero/heroImage.png")}
+        src={getImageUrl("hero/haris.png")}
         alt="Hero image of me"
         className={styles.heroImg}
       />
